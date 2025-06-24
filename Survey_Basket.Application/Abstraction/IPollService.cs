9043bd184a80d6c6045ebf034d6d3afd52e371f5
abanoub Saweris <abanoubphelopos12@gmail.com>
2025-06-24 23:59:20 +0300

    poll service

---
+++ Survey_Basket.Application/Abstraction/IPollService.cs
@@ -0,0 +1,5 @@
+﻿namespace Survey_Basket.Application.Abstraction;
+
+public interface IPollService
+{
+}
---
+++ Survey_Basket.Infrastructure/Implementation/PollService.cs
@@ -0,0 +1,5 @@
+﻿namespace Survey_Basket.Infrastructure.Implementation;
+
+public class PollService
+{
+}
