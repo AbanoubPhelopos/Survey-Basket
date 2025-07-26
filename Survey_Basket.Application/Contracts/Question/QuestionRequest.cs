dec4fe3322630d94888c052878e1f11de64d563c
abanoub Saweris <abanoubphelopos12@gmail.com>
2025-07-26 19:56:42 +0300

    Add Question Contracts

---
+++ Survey_Basket.Application/Contracts/Question/QuestionRequest.cs
@@ -0,0 +1,6 @@
+﻿namespace Survey_Basket.Application.Contracts.Question;
+
+public record QuestionRequest(
+    string Content,
+    List<string> Answers
+);
---
+++ Survey_Basket.Application/Contracts/Question/QuestionRequestValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+
+namespace Survey_Basket.Application.Contracts.Question;
+
+public class QuestionRequestValidator : AbstractValidator<QuestionRequest>
+{
+    public QuestionRequestValidator()
+    {
+        RuleFor(x => x.Content)
+            .NotEmpty()
+            .Length(3, 1000);
+
+        RuleFor(x => x.Answers)
+            .Must(x => x.Count > 1)
+            .WithMessage("At least two answers are required.");
+
+        RuleFor(x => x.Answers)
+            .Must(x => x.Distinct().Count() == x.Count)
+            .WithMessage("Answers must be unique.");
+
+    }
+}
