61cc28e3c8044a355ae1c689a8441ad82e312fe5
abanoub Saweris <abanoubphelopos12@gmail.com>
2025-09-23 20:02:20 +0300

    add start vote endpoint

---
+++ Survey_Basket.Api/Controllers/VotesController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Survey_Basket.Application.Abstraction;
+using Survey_Basket.Application.Errors;
+using Survey_Basket.Application.Services.QuestionServices;
+using System.Security.Claims;
+
+namespace Survey_Basket.Api.Controllers;
+
+[Route("api/polls/{pollId:guid}/[controller]")]
+[ApiController]
+[Authorize]
+public class VotesController : ControllerBase
+{
+    private readonly IQuestionService _questionService;
+    public VotesController(IQuestionService questionService)
+    {
+        _questionService = questionService;
+    }
+    [HttpGet("")]
+    public IActionResult Start([FromRoute] Guid pollId, CancellationToken cancellationToken)
+    {
+        var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+        var result = _questionService.GetAvailableQuestionsAsync(pollId, Guid.Parse(userId!), cancellationToken);
+
+        if (result.Result.IsSuccess)
+        {
+            return Ok(result.Result.Value);
+        }
+        return result.Result.Error.Equals(PollErrors.PollNotFound)
+            ? result.Result.ToProblemDetails(404)
+            : result.Result.ToProblemDetails(409);
+    }
+}
---
+++ Survey_Basket.Application/Extensions/UserExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace Survey_Basket.Application.Extensions;
+
+public static class UserExtensions
+{
+    public static Guid GetUserId(this System.Security.Claims.ClaimsPrincipal user)
+    {
+        var userId = user.FindFirst(System.Security.Claims.ClaimTypes.NameIdentifier)?.Value;
+        return Guid.Parse(userId!);
+    }
+}
